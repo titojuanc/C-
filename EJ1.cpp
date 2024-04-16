742b2d032bab5d220c7b49faa83263ac0ffbca55
titojuanc <alumno26.melgarejo.juan@ipm.edu.ar>
2024-04-16 11:36:39 -0300

    EJ1

---
+++ EJ1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float num1;
+    float num2;
+    cout<<"Ingrese un número: "<<endl;
+    cin>>num1;
+    cout<<"Ingrese otro número: "<<endl;
+    cin>>num2;
+    float r=(num1+num2)/2;
+    cout<<"Su promedio es de: "<<r<<endl;
+}
